7af4beb86b04fe9fecf0b2e62db0976e998096dd
PiZDaBo1 <bulat.fathullin@gmail.ru>
2022-06-21 12:46:22 +0500

    activity2 and boat

--- app/src/main/java/com/example/healt/Activity1.java
+++ app/src/main/java/com/example/healt/Activity1.java
@@ -2,17 +2,43 @@ package com.example.healt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class Activity1 extends AppCompatActivity {
+
+    int[] newArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+
+        newArray = new int[]{
+
+                R.id.boat_pose,R.id.boat_pose2,R.id.boat_pose3,R.id.boat_pose4,R.id.boat1_pose,R.id.boat2_pose,R.id.boat3_pose,R.id.boat4_pose,R.id.boat5_pose,R.id.boat6_pose,R.id.boat7_pose,R.id.boat8_pose,
+
+        };
+
     }
 
     public void ImageBattonCliked(View view) {
+
+        for (int i=0;i< newArray.length;i++){
+
+            if (view.getId() == newArray[i]){
+
+                int value = i+1;
+                Log.i("FIRST", String.valueOf(value));
+                Intent intent = new Intent(Activity1.this, Activity2.class);
+                intent.putExtra("value", String.valueOf(value));
+                startActivity(intent); finish();
+
+            }
+
+        }
+
     }
 }
--- app/src/main/java/com/example/healt/Activity2.java
+++ app/src/main/java/com/example/healt/Activity2.java
@@ -2,13 +2,65 @@ package com.example.healt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Activity2 extends AppCompatActivity {
+
+    String buttenvalue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        Intent intent = getIntent();
+        buttenvalue = intent.getStringExtra("value");
+
+
+        int intvalue = Integer.valueOf(buttenvalue);
+
+
+        switch (intvalue){
+
+            case 1:
+                setContentView(R.layout.activity_boat1);
+                break;
+            case 2:
+                setContentView(R.layout.activity_boat2);
+                break;
+            case 3:
+                setContentView(R.layout.activity_boat3);
+                break;
+            case 4:
+                setContentView(R.layout.activity_boat4);
+                break;
+            case 5:
+                setContentView(R.layout.activity_boat5);
+                break;
+            case 6:
+                setContentView(R.layout.activity_boat6);
+                break;
+            case 7:
+                setContentView(R.layout.activity_boat7);
+                break;
+            case 8:
+                setContentView(R.layout.activity_boat8);
+                break;
+            case 9:
+                setContentView(R.layout.activity_boat9);
+                break;
+            case 10:
+                setContentView(R.layout.activity_boat10);
+                break;
+            case 11:
+                setContentView(R.layout.activity_boat11);
+                break;
+            case 12:
+                setContentView(R.layout.activity_boat12);
+                break;
+
+        }
+
     }
 }
